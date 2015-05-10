759df9edf16d29b197bd9b7a4ea7cff84bde3200
Juan Carlos Suárez <jcsh2006@gmail.com>
2015-05-09 21:48:40 -0500

    db

--- jquery-mobile/zoe.js
+++ jquery-mobile/zoe.js
@@ -10,3 +10,73 @@
 				$(this).trigger('create');
 			});
 		});
+
+
+
+
+            document.addEventListener("deviceready", onDeviceReady, false);
+             
+            var db;
+             
+            function onDeviceReady() {
+				checkDatabase();
+     
+            }
+			
+			function checkDatabase(){
+                db = window.openDatabase("Database", "1.0", "Zoe Database", 2*1024*1024);
+                db.transaction(createDB, errorCB, successCB);
+			}
+         
+            function createDB(tx) {
+                tx.executeSql('CREATE TABLE salesrep (   id_salesrep TEXT NOT NULL,   Name TEXT NOT NULL,   Password TEXT NOT NULL,   isActive INTEGER NOT NULL,   SyincTime NUMERIC NOT NULL,   CONSTRAINT Key2 PRIMARY KEY (id_salesrep),   CONSTRAINT id_salesrep UNIQUE (id_salesrep) )');
+            }
+         
+            function errorCB(err) {
+                alert("Error processing SQL: "+err.code);
+            }
+         
+            function successCB() {
+               alert("YEAH!!!!");
+            }
+         
+            function insertSalesRep(tx) {
+                var _id_salesrep = 1; // $("[name='code']").val();
+                var _Name        = 'Peralta';// $("[name='name']").val();
+                var _Password    = '123456';// $("[name='password']").val();
+                var _isActive    = 1; // $("[name='active']").val();
+                var _SyincTime   = '09/05/2015 09:44:15'; // $("[name='syncronize time']").val();
+				
+                var _description = $("[name='description']").val();
+                var sql = 'INSERT INTO salesrep (id_salesrep, Name, Password, isActive, SyincTime) VALUES (?,?,?,?,?)';
+                tx.executeSql(sql, [_id_salesrep,_Name,_Password,_isActive,_SyincTime], sucessQueryDB, errorCB);
+ 
+            }
+         
+            function sucessQueryDB(tx) {     
+                tx.executeSql('SELECT * FROM salesrep', [], renderList, errorCB);
+            }
+         
+            function renderList(tx,results) {
+                var htmlstring = '';
+                 
+                var len = results.rows.length;
+                 
+                for (var i=0; i<len; i++){
+                    htmlstring += '<li>' + results.rows.item(i).title + '</li>';
+                     
+                }
+                 
+                $('#resultList').html(htmlstring);
+                $('#resultList').listview('refresh');
+                 
+                 
+            }
+ 
+            function submitForm() {
+                db.transaction(insertDB, errorCB);
+                $.mobile.changePage( "#page2", { reverse: false, transition: "slide" } );
+                return false;
+            }
+ 
+ 
