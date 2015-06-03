0b86de3a61f84d62d6944f3b2fe6758e07718999
JuanFernando <juan.fernando.londono@gmail.com>
2015-06-02 19:15:15 -0500

    aaa

--- dbcreate.sql
+++ dbcreate.sql
@@ -1,6 +1,6 @@
 ﻿/*
 Created: 29/04/2015
-Modified: 31/05/2015
+Modified: 02/06/2015
 Model: RE SQLite 3.7
 Database: SQLite 3.7
 */
@@ -10,19 +10,24 @@ Database: SQLite 3.7
 
 -- Drop indexes section -------------------------------------------------
 
-DROP INDEX IF EXISTS IX_invoice_custumer;
-DROP INDEX IF EXISTS customer_idx1;
-DROP INDEX IF EXISTS IX_sales_rep_customer;
-DROP INDEX IF EXISTS IX_Relationship7;
-DROP INDEX IF EXISTS idx_salesrep_1;
+DROP INDEX IX_invoice_lines;
+DROP INDEX IX_Relationship1;
+DROP INDEX IX_Relationship2;
+DROP INDEX IX_invoice_custumer;
+DROP INDEX IX_invoice_terms;
+DROP INDEX customer_idx1;
+DROP INDEX IX_sales_rep_customer;
+DROP INDEX idx_salesrep_1;
 
 -- Drop tables section ---------------------------------------------------
 
-DROP TABLE IF EXISTS terms;
-DROP TABLE IF EXISTS invoice_item;
-DROP TABLE IF EXISTS invoice;
-DROP TABLE IF EXISTS customer;
-DROP TABLE IF EXISTS salesrep;
+DROP TABLE salesTax;
+DROP TABLE Inventory;
+DROP TABLE terms;
+DROP TABLE invoice_item;
+DROP TABLE invoice;
+DROP TABLE customer;
+DROP TABLE salesrep;
 
 -- Create tables section -------------------------------------------------
 
@@ -35,7 +40,7 @@ CREATE TABLE salesrep
   Password TEXT NOT NULL,
   isActive INTEGER NOT NULL,
   SyncTime NUMERIC NOT NULL,
-  Initial TEXT,
+  initials TEXT,
   CONSTRAINT Key2 PRIMARY KEY (id_salesrep),
   CONSTRAINT id_salesrep UNIQUE (id_salesrep)
 );
@@ -59,14 +64,14 @@ CREATE TABLE customer
   cellPhone TEXT,
   email TEXT,
   shipAddressZipcode TEXT,
-  billAddresZipcode TEXT,
+  billAddresZipCode TEXT,
   billAddresCity TEXT,
   billAddressState TEXT,
   billAddressCountry TEXT,
   shipAddressCity TEXT,
   shipAddressState TEXT,
   shipAddressCountry TEXT,
-  id_salesrep TEXT,
+  id_salesrep TEXT NOT NULL,
   routeDay1 INTEGER,
   routeDay2 INTEGER,
   routeDay3 INTEGER,
@@ -75,22 +80,19 @@ CREATE TABLE customer
   routeDay6 INTEGER,
   routeDay7 INTEGER,
   Fax TEXT,
+  companyName TEXT,
   billAddress3 TEXT,
   shipAddress3 NONE,
   name TEXT,
   companyName TEXT,
   otherDetails TEXT,
-  id_term TEXT,
   CONSTRAINT Key3 PRIMARY KEY (ListID),
-  CONSTRAINT sales_rep_customer FOREIGN KEY (id_salesrep) REFERENCES salesrep (id_salesrep),
-  CONSTRAINT Relationship7 FOREIGN KEY (id_term) REFERENCES terms (id_term)
+  CONSTRAINT sales_rep_customer FOREIGN KEY (id_salesrep) REFERENCES salesrep (id_salesrep)
 );
 
 CREATE INDEX customer_idx1 ON customer (FullName);
 
 CREATE INDEX IX_sales_rep_customer ON customer (id_salesrep);
-
-CREATE INDEX IX_Relationship7 ON customer (id_term);
 
 -- Table invoice
 
@@ -119,23 +121,41 @@ CREATE TABLE invoice
   salesTaxTotal NUMERIC,
   shipDate INTEGER,
   subtotal NUMERIC,
+  id_term TEXT NOT NULL,
   billAddress_addr3 TEXT,
   shipAddress_addr3 TEXT,
   CONSTRAINT Key4 PRIMARY KEY (id_invoice),
-  CONSTRAINT invoice_custumer FOREIGN KEY (ListID) REFERENCES customer (ListID)
+  CONSTRAINT invoice_custumer FOREIGN KEY (ListID) REFERENCES customer (ListID),
+  CONSTRAINT invoice_terms FOREIGN KEY (id_term) REFERENCES terms (id_term)
 );
 
 CREATE INDEX IX_invoice_custumer ON invoice (ListID);
+
+CREATE INDEX IX_invoice_terms ON invoice (id_term);
 
 -- Table invoice_item
 
 CREATE TABLE invoice_item
 (
-  id_item TEXT NOT NULL,
+  LineID TEXT NOT NULL,
   id_invoice TEXT NOT NULL,
-  CONSTRAINT Key5 PRIMARY KEY (id_item,id_invoice),
-  CONSTRAINT invoice_lines FOREIGN KEY (id_invoice) REFERENCES invoice (id_invoice)
+  Inventory_ListID TEXT,
+  Desc TEXT,
+  Quantity NUMERIC,
+  Rate NUMERIC,
+  Amount NUMERIC,
+  SalesTax_ListID TEXT,
+  CONSTRAINT Key5 PRIMARY KEY (LineID),
+  CONSTRAINT invoice_lines FOREIGN KEY (id_invoice) REFERENCES invoice (id_invoice),
+  CONSTRAINT Relationship1 FOREIGN KEY (Inventory_ListID) REFERENCES Inventory (ListID),
+  CONSTRAINT Relationship2 FOREIGN KEY (SalesTax_ListID) REFERENCES salesTax (ListID)
 );
+
+CREATE INDEX IX_invoice_lines ON invoice_item (id_invoice);
+
+CREATE INDEX IX_Relationship1 ON invoice_item (Inventory_ListID);
+
+CREATE INDEX IX_Relationship2 ON invoice_item (SalesTax_ListID);
 
 -- Table terms
 
@@ -144,6 +164,24 @@ CREATE TABLE terms
   id_term TEXT NOT NULL,
   fullName TEXT,
   CONSTRAINT Key6 PRIMARY KEY (id_term)
+);
+
+-- Table Inventory
+
+CREATE TABLE Inventory
+(
+  ListID TEXT NOT NULL,
+  FullName TEXT,
+  CONSTRAINT Key7 PRIMARY KEY (ListID)
+);
+
+-- Table salesTax
+
+CREATE TABLE salesTax
+(
+  ListID TEXT NOT NULL,
+  FullName TEXT,
+  CONSTRAINT Key8 PRIMARY KEY (ListID)
 );
 
 
---
+++ js/zoe/invoiceDAO.js
@@ -0,0 +1,115 @@
+// JavaScript Document
+
+var invoiceDAO = {listBySalesrep:listInvoicesBySalesrep, 
+				getById:getInvoiceById, 
+				store:storeInvoice, 
+				deleteAll:deleteAllInvoices};
+var filterDataInvoice;
+var invoiceReceiveFunction;
+var invoiceReceiveListFunction;
+var invoiceErrFunc;
+var invoiceVO;
+var recordInvoice;
+
+//----------------------
+//metodos hacia afuera
+//----------------------
+function getInvoiceById(aId,aReceiveFunction,aErrFunc){
+	db = openDatabaseZoe();
+	logZoe("getInvoice db=" + db);
+	filterData=aId;
+	invoiceReceiveFunction = aReceiveFunction;
+	invoiceErrFunc = aErrFunc;
+	db.transaction(doSelectInvoice, invoiceErrFunc, invoiceReceiveFunction);
+}
+
+function listInvoicesBySalesRep(customer_ListID, aReceiveFunction,aErrFunc){
+	db = openDatabaseZoe();
+	logZoe("listInvoices db=" + db);
+	invoiceReceiveListFunction = aReceiveFunction;
+	invoiceErrFunc = aErrFunc;
+	filterDataInvoice = customer_ListID;
+	db.transaction(doCustomerInvoices, invoiceErrFunc, invoiceLocalListReceiveFunction);
+}
+
+function storeInvoice(records,aErrFunc,successCB){
+	db = openDatabaseZoe();
+	logZoe("storeInvoice db=" + db);
+	recordInvoice = records;
+	invoiceErrFunc = aErrFunc;
+	db.transaction(doStoreInvoice, errorCB, successCB);
+}
+
+function deleteAllInvoice(aErrFunc,successCB){
+	db = openDatabaseZoe();
+	logZoe("deleteAllInvoice db=" + db);
+	invoiceErrFunc = aErrFunc;
+	db.transaction(doDeleteAllInvoice, errorCB, successCB);
+}
+
+
+//----------------------
+//metodos privados
+//----------------------
+
+function doSelectInvoice(tx){
+	logZoe("doSelectInvoice filterData=" + filterDataInvoice);
+	tx.executeSql("SELECT id_invoice, ListID, po_number, dueDate, appliedAmount, balanceRemaining, billAddress_addr1, billAddress_addr2, billAddress_addr3, billAddress_city, billAddress_state, billAddress_postalcode, shipAddress_addr1, shipAddress_addr2, shipAddress_addr3, shipAddress_city, shipAddress_state, shipAddress_postalcode, isPaid, isPending, refNumber, salesTaxPercentage, salesTaxTotal, shipDate, subtotal FROM invoice Where id_invoice = ?", [filterDataInvoice],invoiceLocalReceiveFunction, invoiceErrFunc);
+}
+
+function doCustomerInvoices(tx){
+	logZoe("doSelectSelesrepInvoices");
+	tx.executeSql("SELECT id_invoice, ListID, po_number, dueDate, appliedAmount, balanceRemaining, billAddress_addr1, billAddress_addr2, billAddress_addr3, billAddress_city, billAddress_state, billAddress_postalcode, shipAddress_addr1, shipAddress_addr2, shipAddress_addr3, shipAddress_city, shipAddress_state, shipAddress_postalcode, isPaid, isPending, refNumber, salesTaxPercentage, salesTaxTotal, shipDate, subtotal FROM invoice WHERE ListID = ?", [filterDataInvoice],invoiceLocalListReceiveFunction, invoiceErrFunc);
+}
+
+function invoiceLocalReceiveFunction(tx,results){
+	logZoe("invoiceLocalReceiveFunction results = " + JSON.stringify(results));
+	if (results.rows.length>0){
+	logZoe("localReceiveFunction1 " + JSON.stringify(results.rows.item(0)));
+		invoiceVO=results.rows.item(0);
+		invoiceReceiveFunction(invoiceVO);
+	}
+	logZoe("localReceiveFunction fin");
+}
+
+function invoiceLocalListReceiveFunction(tx,results){
+	logZoe("invoiceLocalListReceiveFunction results.length=" + results.rows.length);
+	var i;
+	var arrayInvoices = new Array();
+	for (i=0;i<results.rows.length;i++){
+	logZoe("invoiceLocalListReceiveFunction " + JSON.stringify(results.rows.item(0)));
+		arrayInvoices[i] = results.rows.item(i);
+	}
+	invoiceReceiveListFunction(arrayInvoices);
+}
+
+function doStoreInvoice(tx){
+	logZoe ("doStoreInvoice ");
+	if (recordInvoice.length){
+		var i;
+		for (i=0;i<recordInvoice.length;i++){
+			var theRecord = recordInvoice[i];
+			logZoe("store invoice:" + JSON.stringify(theRecord));
+			doStoreOneInvoice(tx, theRecord);
+		}
+	}else{
+			doStoreOneInvoice(tx, recordInvoice);
+	}
+	
+}
+
+function doStoreOneInvoice(tx, rec){
+	tx.executeSql('INSERT OR REPLACE INTO invoice(id_invoice, ListID, po_number, dueDate, appliedAmount, balanceRemaining, billAddress_addr1, billAddress_addr2, billAddress_addr3, billAddress_city, billAddress_state, billAddress_postalcode, shipAddress_addr1, shipAddress_addr2, shipAddress_addr3, shipAddress_city, shipAddress_state, shipAddress_postalcode, isPaid, isPending, refNumber, TaxPercentage, salesTaxTotal, shipDate, subtotal) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[rec.id_invoice, rec.ListID, rec.po_number, rec.dueDate, rec.appliedAmount, rec.balanceRemaining, rec.billAddress_addr1, rec.billAddress_addr2, rec.billAddress_addr3, rec.billAddress_city, rec.billAddress_state, rec.billAddress_postalcode, rec.shipAddress_addr1, rec.shipAddress_addr2, rec.shipAddress_addr3, rec.shipAddress_city, rec.shipAddress_state, rec.shipAddress_postalcode, rec.isPaid, rec.isPending, rec.refNumber, rec.TaxPercentage, rec.salesTaxTotal, rec.shipDate, rec.subtotal]);
+	
+ if (rec.items){
+	 for (var i=0;i<rec.items[i];i++){
+		 var item = rec.item;
+		 tx.executeSql('INSERT OR REPLACE INTO invoice_item(LineID,id_invoice,Inventory_ListID,Desc,Quantity,Rate,Amount,SalesTax_ListID) VALUES(?,?,?,?,?,?,?,?)',[item.LineID,item.id_invoice,item.Inventory_ListID,item.Desc,item.Quantity,item.Rate,item.Amount,item.SalesTax_ListID]);
+	 }
+ }
+}
+
+function doDeleteAllInvoices(tx){
+	tx.executeSql('DELETE FROM invoice',[]);
+	tx.executeSql('DELETE FROM invoice_items',[]);
+}
