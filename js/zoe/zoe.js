36e6c397e22f8ca37e29bd502baa23e02fc7df49
JuanFernando <juan.fernando.londono@gmail.com>
2015-06-09 14:38:43 -0500

    jflc

--- js/zoe/zoe.js
+++ js/zoe/zoe.js
@@ -189,3 +189,14 @@ function ifUndefNull(variable){
 	}
 	return variable;
 }
+
+
+function generateKey(){
+	var date = new Date();
+	var toReturn = date.getTime();
+	toReturn += 'xxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+	return toReturn;
+}
